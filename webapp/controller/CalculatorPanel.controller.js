67fb5c8f0c1e9c45f4860da770f99e89a5c070d4
Simeon <simeontfilev@gmail.com>
2020-05-03 16:21:17 +0300

    ui5-calculator

---
+++ webapp/controller/CalculatorPanel.controller.js
@@ -0,0 +1,55 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+], function (Controller, MessageToast) {
+	"use strict";
+
+	return Controller.extend("sap.ui.acad.calculator.controller", {
+
+		onShowHello : function () {
+			MessageToast.show(sMsg);
+        },
+        calculate:function(){
+            var sExpression = this.getView().getModel().getProperty("/calculator/expression");
+            
+            var sEndpoint = "https://restcalculator.cfapps.eu10.hana.ondemand.com/calculator/expressions";
+            var params = {
+                expression: sExpression
+            };
+            var sURL = sEndpoint + formatParams(params);
+            
+            var oxhr = new XMLHttpRequest();
+            oxhr.open("POST", sURL, true);
+            oxhr.send();
+            var oModel =  this.getView().getModel();
+            oxhr.onerror = function(){
+              oModel.setProperty("/calculator/answer","INVALID EXPRESSION");
+              oModel.setProperty("/calculator/expression","");
+            }
+            oxhr.onload = function() {
+                if(oxhr.status == 200){
+                  var oJSONData = JSON.parse(this.responseText);
+                  var dAnswer = oJSONData['answer'];
+                  oModel.setProperty("/calculator/answer",dAnswer);
+                }
+              }
+        },
+        addToExpression:function(e){
+            var sExpression = this.getView().getModel().getProperty("/calculator/expression");
+            var oButton = e.getSource();
+            var sButtonText = oButton.mProperties.text;
+            var sNewExpression = sExpression + sButtonText;
+            this.getView().getModel().setProperty("/calculator/expression",sNewExpression);
+           
+        }
+
+	});
+
+});
+function formatParams( params ){
+    return "?" + Object
+          .keys(params)
+          .map(function(key){
+            return key+"="+encodeURIComponent(params[key])
+          })
+          .join("&")
+  }
